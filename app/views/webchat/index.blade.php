1b68df9d3654c08a5ed55a297840fe3598736b37
王金鹏 <firKin@126.com>
2014-12-24 09:27:20 +0800

    fixed

--- app/views/webchat/index.blade.php
+++ app/views/webchat/index.blade.php
@@ -11,19 +11,20 @@
 
 <div class="layout style0 yxl">
 	<div class="hd">
-		<h2>影像力</h2>
+		<h2>重邮小帮手微信活动展示</h2>
 		<b class="border"></b>
-		<a href="javascript:;" class="changeBnt" id="xxlChg"><i></i>换一换</a>
+		<!-- <a href="javascript:;" class="changeBnt" id="xxlChg"><i></i>换一换</a> -->
+		<img class="changeBnt" src="img/96043874.jpg" alt="" />
 	</div>
 	<div class="bd">
 		<ul class="picLB" id="picLBxxl">
 			<li>
 				<dl class="picDl huozhe">
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/114236462.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="tupian"><img src="img/114140276.jpg" alt=""/></a>
 						<div class="ftBox">
 							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">父背如山</a></div>
+							<div class="tit"><a href="javascript:;">父背如山</a></div>
 							<div class="text">父亲背上的求职路。</div>
 							<div class="qr">
 								<i class="icon-arr"></i>
@@ -33,7 +34,7 @@
 						</div>
 					</dd>
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/114140276.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="erweima"><img src="img/113842142.jpg" alt=""/></a>
 						<div class="ftBox">
 							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
 							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">七娃之家</a></div>
@@ -45,12 +46,29 @@
 							</div>
 						</div>
 					</dd>
+				</dl>
+			</li>
+			<li>
+				<dl class="picDl huozhe">
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/114011457.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="tupian"><img src="img/113584996.jpg" alt=""/></a>
 						<div class="ftBox">
 							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">毛家工业园</a></div>
-							<div class="text">城市化进程中的暴富神话</div>
+							<div class="tit"><a href="javascript:;">父背如山</a></div>
+							<div class="text">父亲背上的求职路。</div>
+							<div class="qr">
+								<i class="icon-arr"></i>
+								<img class="fl" src="img/113842142.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
+							</div>
+						</div>
+					</dd>
+					<dd>
+						<a href="javascript:;" class="pic" id="erweima"><img src="img/113685865.jpg" alt=""/></a>
+						<div class="ftBox">
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">七娃之家</a></div>
+							<div class="text">即使贫困，七个孩子也都要养</div>
 							<div class="qr">
 								<i class="icon-arr"></i>
 								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
@@ -61,156 +79,121 @@
 				</dl>
 			</li>
 			<li>
-				<dl class="picDl jilu">
+				<dl class="picDl huozhe">
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/114097562.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="tupian"><img src="img/113064706.jpg" alt=""/></a>
 						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">焦点人物</a></b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">任性的音乐人帕尔哈提</a></div>
-							<div class="text">自由与音乐是他的追求。</div>
-						</div>
-					</dd>
-					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/113842142.jpg" alt=""/></a>
-						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">焦点人物</a></b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">被泼粪的性学教授</a></div>
-							<div class="text">反黄大妈激烈抵制普及性科学</div>
-						</div>
-					</dd>
-					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/113584996.jpg" alt=""/></a>
-						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">焦点人物</a></b></h2>
-							<div class="tit"><a  href="http://hongyan.cqupt.edu.cn/">穿越罗布泊的6岁男孩</a></div>
-							<div class="text">多多接受的"鹰式教育"引发争议</div>
-						</div>
-					</dd>
-				</dl>
-			</li>
-			<li>
-				<dl class="picDl tuhua">
-					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/114273547.jpg" alt=""/></a>
-						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">图话</a></b></h2>
-							<div class="tit"><a  href="http://hongyan.cqupt.edu.cn/">靠不住的“总统”</a></div>
-							<div class="text">他们被挑战、被监督、被挑剔</div>
-						</div>
-					</dd>
-					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img nsrc="img/114273557.jpg" alt=""/></a>
-						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">图话</a></b></h2>
-							<div class="tit"><a  href="http://hongyan.cqupt.edu.cn/">天安门广场进化史</a></div>
-							<div class="text">广场改造折射出国家政治变迁</div>
-						</div>
-					</dd>
-					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img nsrc="img/114099210.jpg" alt=""/></a>
-						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">图话</a></b></h2>
-							<div class="tit"><a  href="http://hongyan.cqupt.edu.cn/">老人为何孤独死去？</a></div>
-							<div class="text">在注重礼义仁孝的国度，老人却悲凉而死？</div>
-						</div>
-					</dd>
-				</dl>
-			</li>
-			<li>
-				<dl class="picDl today">
-					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/114270241.jpg" alt=""/></a>
-						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">中国人的一天</a></b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">盲人调律师刘佳明</a></div>
-							<div class="text">第1805期</div>
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="javascript:;">父背如山</a></div>
+							<div class="text">父亲背上的求职路。</div>
 							<div class="qr">
 								<i class="icon-arr"></i>
-								<img class="fl" src="img/96091063.png" alt="中国人的一天微信" />
-								<div class="text">扫二维码 <br />收听《中国人的一天》微信</div>
+								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
 							</div>
 						</div>
 					</dd>
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img nsrc="img/114230287.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="erweima"><img src="img/113366099.jpg" alt=""/></a>
 						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">中国人的一天</a></b></h2>
-							<div class="tit"><a  href="http://hongyan.cqupt.edu.cn/">陈正华和他的小戏楼</a></div>
-							<div class="text">第1804期</div>
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">七娃之家</a></div>
+							<div class="text">即使贫困，七个孩子也都要养</div>
 							<div class="qr">
 								<i class="icon-arr"></i>
-								<img class="fl" src="img/96091063.png" alt="中国人的一天微信" />
-								<div class="text">扫二维码 <br />收听《中国人的一天》微信</div>
-							</div>
-						</div>
-					</dd>
-					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img nsrc="img/114188359.jpg" alt=""/></a>
-						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">中国人的一天</a></b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">八旬老人抚养孤儿</a></div>
-							<div class="text">第1803期</div>
-							<div class="qr">
-								<i class="icon-arr"></i>
-								<img class="fl" src="img/96091063.png" alt="中国人的一天微信" />
-								<div class="text">扫二维码 <br />收听《中国人的一天》微信</div>
+								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
 							</div>
 						</div>
 					</dd>
 				</dl>
 			</li>
 			<li>
-				<dl class="picDl yingzhan">
+				<dl class="picDl huozhe">
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/114172169.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="tupian"><img src="img/113981312.jpg" alt=""/></a>
 						<div class="ftBox">
-							<h2 class="name"><b> <a href="http://hongyan.cqupt.edu.cn/">在线影展</a> </b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">不再独自旅行</a></div>
-							<div class="text">因为投缘所以两对couple相约一起去澳洲。</div>
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="javascript:;">父背如山</a></div>
+							<div class="text">父亲背上的求职路。</div>
+							<div class="qr">
+								<i class="icon-arr"></i>
+								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
+							</div>
 						</div>
 					</dd>
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img nsrc="img/113366099.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="erweima"><img src="img/114011457.jpg" alt=""/></a>
 						<div class="ftBox">
-							<h2 class="name"><b> <a href="http://hongyan.cqupt.edu.cn/">在线影展</a> </b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">行走德黑兰</a></div>
-							<div class="text">玫瑰之城德黑兰，神秘，却又风情万种。</div>
-						</div>
-					</dd>
-					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img nsrc="img/113064706.jpg" alt=""/></a>
-						<div class="ftBox">
-							<h2 class="name"><b> <a href="http://hongyan.cqupt.edu.cn/">在线影展</a> </b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">墙上的伟人</a></div>
-							<div class="text">记录陕北家庭房间中领导人宣传画。</div>
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">七娃之家</a></div>
+							<div class="text">即使贫困，七个孩子也都要养</div>
+							<div class="qr">
+								<i class="icon-arr"></i>
+								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
+							</div>
 						</div>
 					</dd>
 				</dl>
 			</li>
 			<li>
-				<dl class="picDl jingxuan">
+				<dl class="picDl huozhe">
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img src="img/114159822.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="tupian"><img src="img/114097562.jpg" alt=""/></a>
 						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">一周精选</a></b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">2014.11.29-12.05</a></div>
-							<div class="text">马路车技</div>
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="javascript:;">父背如山</a></div>
+							<div class="text">父亲背上的求职路。</div>
+							<div class="qr">
+								<i class="icon-arr"></i>
+								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
+							</div>
 						</div>
 					</dd>
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img nsrc="img/113981312.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="erweima"><img src="img/114099210.jpg" alt=""/></a>
 						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">一周精选</a></b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">2014.11.22-11.28</a></div>
-							<div class="text">法国牧羊人将羊群赶至埃菲尔铁塔下示威。</div>
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">七娃之家</a></div>
+							<div class="text">即使贫困，七个孩子也都要养</div>
+							<div class="qr">
+								<i class="icon-arr"></i>
+								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
+							</div>
+						</div>
+					</dd>
+				</dl>
+			</li>
+			<li>
+				<dl class="picDl huozhe">
+					<dd>
+						<a href="javascript:;" class="pic" id="tupian"><img src="img/114159822.jpg" alt=""/></a>
+						<div class="ftBox">
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="javascript:;">父背如山</a></div>
+							<div class="text">父亲背上的求职路。</div>
+							<div class="qr">
+								<i class="icon-arr"></i>
+								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
+							</div>
 						</div>
 					</dd>
 					<dd>
-						<a href="http://hongyan.cqupt.edu.cn/" class="pic"><img nsrc="img/113685865.jpg" alt=""/></a>
+						<a href="javascript:;" class="pic" id="erweima"><img src="img/114172169.jpg" alt=""/></a>
 						<div class="ftBox">
-							<h2 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">一周精选</a></b></h2>
-							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">2014.11.15—11.21</a></div>
-							<div class="text">白马群在夕阳下奔跑</div>
+							<h3 class="name"><b><a href="http://hongyan.cqupt.edu.cn/">活着</a></b></h3>
+							<div class="tit"><a href="http://hongyan.cqupt.edu.cn/">七娃之家</a></div>
+							<div class="text">即使贫困，七个孩子也都要养</div>
+							<div class="qr">
+								<i class="icon-arr"></i>
+								<img class="fl" src="img/96043874.jpg" alt="活着微信" />
+								<div class="text">扫二维码 <br />收听《活着》微信</div>
+							</div>
 						</div>
 					</dd>
 				</dl>
